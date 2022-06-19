e43caf78f79992a2bb9f700be73b929dcb2e440c
Alan Barros <alan.p.barros@gmail.com>
2022-06-19 15:06:53 -0300

    update pkg-helper.sh

--- pkg-helper.sh
+++ pkg-helper.sh
@@ -2,13 +2,20 @@
 
 #Example: sh pkg-helper.sh -l alanbarros -k $(cat ../key.txt) -v 0.0.7
 
-PROJECT_NAME=SpreadSheetParser
-VERSION=0.0.5
-DIR=src/bin/Release
+WORK_DIR=src
+PATH_CSPROJ=$WORK_DIR/SpreadSheetParser.csproj
+PROJECT_NAME=$(xmlstarlet sel -t -v '//PackageId' $PATH_CSPROJ)
+VERSION=$(xmlstarlet sel -t -v '//Version' $PATH_CSPROJ)
+CONFIG=Release
+DIR=$WORK_DIR/bin/$CONFIG
 
 for x in ${@} # Organiza os argumentos 
 do
     case ${x} in
+    -c | --configuration)
+        CONFIG=${2}
+        shift 2
+        ;;
     -l | --login)
         LOGIN=${2}
         shift 2
@@ -43,10 +50,10 @@ echo "Substituindo tokens e criando arquivo nuget.config"
 sed "s/{{PAT}}/${KEY}/g" templateNuget.config | sed "s/{{USER}}/${USER}/g" > nuget.config
 
 echo "Limpando, restaurando, compilando e empacotando o projeto"
-dotnet clean src/${PROJECT_NAME}.csproj
-dotnet restore src/${PROJECT_NAME}.csproj
-dotnet build src/${PROJECT_NAME}.csproj
-dotnet pack src/${PROJECT_NAME}.csproj --configuration Release
+dotnet clean $PATH_CSPROJ
+dotnet restore $PATH_CSPROJ
+dotnet build $PATH_CSPROJ
+dotnet pack $PATH_CSPROJ --configuration $CONFIG -o $DIR
 
 if [ ! -z ${SHOULD_PUBLISH} ]; then # Publica o pacote
 echo "Publicando ${PACKAGE} no Github Packages...";
